ef8265a3dc90468286441967ad0c487c57d986cf
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-04 17:45:39 +0530

    ConutAndRemoveSpaces +

---
+++ Interview_Programs/src/info/ashutosh/ConutAndRemoveSpaces.java
@@ -0,0 +1,17 @@
+package info.ashutosh;
+
+import java.util.function.Function;
+
+public class ConutAndRemoveSpaces {
+
+	public static void main(String[] args) {
+		String text = "a bb ccc dddd";
+		Function<String, Integer> spaceCount = (s) -> s.length() - s.replaceAll(" ", "").length();
+		Integer apply = spaceCount.apply(text);
+		System.out.println("Total Spaces :"+apply);
+		System.out.println("Original Test :"+text);
+		System.out.println("String Without Spaces :"+text.replaceAll(" ", ""));
+
+	}
+
+}
